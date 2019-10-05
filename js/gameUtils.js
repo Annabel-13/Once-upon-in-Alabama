b3155415e10caa2f9eec3bee769df3ca0c4b26d6
Анна <AnnabelFear@gmail.com>
2019-10-05 16:49:01 +0300

    new js file

--- js/gameUtils.js
+++ js/gameUtils.js
@@ -1,106 +1,29 @@
+function createCactus(maxValue, mainDiv, dictionary) {
 
+    let offset = 10;
 
-class GameScore extends BaseDecoration{
+    for(let i = 1; i< 10; i++){
 
+        let size = 300;
+        let marginBottom = getRandValue(offset, maxValue - (2 * offset));
+        let marginLeft = getRandValue(0, window.innerWidth - size);
 
-    constructor(mainDiv, text) {
-        super(mainDiv);
-        this.div.innerText = text;
-    }
-
-    setMargins(){
-        this.div.style.bottom = (window.innerHeight/ 2) - 100 + "px";
-        this.div.style.left =  (window.innerWidth / 2) - 200 + "px";
-    }
-
-
-    //этот метод создает див и возвращает
-    createChildDiv() {
-        let div = document.createElement('div');
-            div.classList.add("gameScore");
-        return div;
-    }
-
-    //здесь мы должны создать табличку, она выплывет на середину и заиграет музыка
-   showTable(text){
-        this.div.innerText = text;
-        this.scoreAppearAnimation();
-        AudioHelper.getInstance().playScore();
-    }
-
-
-    scoreAppearAnimation(){
-
-        let opacity =  Number(this.div.style.opacity);
-
-        let id = setInterval(() => {
-            if(opacity < 10){
-                opacity += 1;
-                this.div.style.opacity = opacity / 10;
-            }else {
-                clearInterval(id);
-            }
-        }, 100);
-
-    }
+        let cactus = new Cactus(mainDiv, dictionary, "cactus"+ i);
+        cactus.setMargins(marginLeft + "px", marginBottom + "px");
 
 
 
+    }}
 
+function appearTarget(mainDiv, dictionary,tag, healthTable){
+    let engine = new Engine(mainDiv, dictionary, tag, healthTable);
+    engine.setMargins(window.innerWidth);
+    engine.moveGun(document);
+    engine.preparedGun(dictionary);
+    engine.preparedReloadGun();
 }
 
 
-//getGroundHeight = () => { return window.innerHeight / 10; };
-class Panorama {
-
-    static createMainDiv() {
-        let ground = document.createElement('div');
-            ground.classList.add("ground");
-
-        let mainDiv = document.createElement('div');
-            mainDiv.classList.add("panorama");
-            mainDiv.appendChild(ground);
-        return mainDiv;
-    }
+function getRandValue(minValue, maxValue) {
+    return  Math.floor(Math.random() * maxValue) + minValue;
 }
-
-class HealthScore extends BaseDecoration{
-
-
-    maxHealthValue = 100;
-    textDiv;
-    contentDiv;
-
-    constructor(mainDiv) {
-        super(mainDiv);
-
-        this.textDiv = document.createElement('div');
-        this.textDiv.classList.add("healthScore-textDiv");
-        this.textDiv.innerText = this.maxHealthValue + "%";
-
-        this.contentDiv = document.createElement('div');
-        this.contentDiv.classList.add("healthScore-contentDiv");
-
-        this.div.append(this.contentDiv);
-        this.div.append(this.textDiv);
-    }
-
-
-    createChildDiv(){
-        let healthScore = document.createElement('div');
-            healthScore.classList.add("healthScore");
-        return healthScore;
-
-    }
-
-
-
-    setHealthValue(currentHealthValue){
-        this.textDiv.innerText = currentHealthValue + "%";
-        this.contentDiv.style.width =  currentHealthValue * (300 / this.maxHealthValue) + "px";
-        this.contentDiv.style.backgroundColor = currentHealthValue > 50 ? "#0ba754": currentHealthValue > 33 ? "#f8ef48" : "#f83837";
-    }
-
-
-}
-
--- js/index.js
+++ js/index.js
@@ -46,33 +46,5 @@ document.body.style.cursor = "url('images/smallTarget.png'), auto";
 
 
 
-function createCactus(maxValue, mainDiv, dictionary) {
 
-        let offset = 10;
-
-    for(i = 1; i< 10; i++){
-
-        let size = 300;
-        let marginBottom = getRandValue(offset, maxValue - (2 * offset));
-        let marginLeft = getRandValue(0, window.innerWidth - size);
-
-        let cactus = new Cactus(mainDiv, dictionary, "cactus"+ i);
-        cactus.setMargins(marginLeft + "px", marginBottom + "px");
-
-
-
-}}
-
-function appearTarget(mainDiv, dictionary,tag, healthTable){
-    let engine = new Engine(mainDiv, dictionary, tag, healthTable);
-        engine.setMargins(window.innerWidth);
-        engine.moveGun(document);
-        engine.preparedGun(dictionary);
-        engine.preparedReloadGun();
-}
-
-
-function getRandValue(minValue, maxValue) {
-   return  Math.floor(Math.random() * maxValue) + minValue;
-}
 
